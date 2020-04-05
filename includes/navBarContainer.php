a019a6ac01217bb907dffd0e2ce577eed7a9af0c
Andrew-Causelof <akozlov@rocketmail.com>
2020-04-05 22:19:19 +0300

    Created and filled database, also finished index.php page

---
+++ includes/navBarContainer.php
@@ -0,0 +1,30 @@
+<div id="navBarContainer">
+	<nav class="navBar">
+		<a href="index.php" class="Logo">
+			<img src="assets/images/icons/logo.png" alt="Logo">
+		</a>	
+
+		<div class="group">		
+			<div class="naveItem">
+				<a href="search.php" class="navItemLink">Search
+					<img src="assets/images/icons/search.png" alt="Search" class="icon">
+							</a>
+			</div>
+		</div>
+
+		<div class="group">
+			<div class="naveItem">
+				<a href="browse.php" class="navItemLink">Browse</a>
+			</div>
+
+			<div class="naveItem">
+				<a href="yourMusic.php" class="navItemLink">Your Music</a>
+			</div>
+
+			<div class="naveItem">
+				<a href="profile.php" class="navItemLink"> User Name</a>
+			</div>
+
+		</div>
+	</nav>	
+</div>
--- index.php
+++ index.php
@@ -1,21 +1,29 @@
+<?php include("includes/header.php"); ?>
 
-<?php
+<h1 class="pageHeadingBig"> You Might Also Like </h1>
 
-include("includes/config.php");
+	<div class="gridViewContainer">
+		<?php
+			$albumQuery = mysqli_query($con," SELECT * FROM albums ORDER BY RAND() LIMIT 10");
+			while($row= mysqli_fetch_array($albumQuery)) {
 
-if(isset($_SESSION['userLoggedIn'])){
-	$userLoggedIn = $_SESSION['userLoggedIn'];
-} else {
-	header("Location: register.php");
-}
-?>
+				
+				echo "<div class='gridViewItem'> 
+					<a href= 'album.php?id=". $row['id'] ."'>
+					<img src='". $row['artworkPath'] ."'>
+					<div class='gridViewInfo'>"
+						. $row['title'] .
+					"</div>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Welcome to Slotify!</title>
-</head>
-<body>
-	Hello!
-</body>
-</html>
+				</div>";
+			}
+		?>
+	</div>
+
+
+
+
+
+<?php include("includes/footer.php") ?>
+
+
